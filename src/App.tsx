f92732e994f84ea3b81f68192555cc7bd02902f1
pmealha <ped.mealha@gmail.com>
2023-12-28 22:11:18 +0000

    increment and decrement implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import './App.css';
 import Countdown from './components/countdown.component';
 
 const App = () => {
-	// return <Countdown startNumber={10} endNumber={20} />;
-	return <Countdown startNumber={20} endNumber={10} />;
+	return <Countdown startNumber={95} endNumber={105} />;
 };
 
 export default App;
--- src/components/countdown.component.tsx
+++ src/components/countdown.component.tsx
@@ -1,5 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './countdown.css';
+
+interface DialProps {
+	digit: string;
+	isIncrementing: boolean;
+}
+
+const Dial: React.FC<DialProps> = ({ digit, isIncrementing }) => {
+	const [rotation, setRotation] = useState(-36 * parseInt(digit, 10));
+
+	useEffect(() => {
+		setRotation((prevRotation) => {
+			const expectedRotation = -36 * parseInt(digit, 10);
+			let rotationIncrement = expectedRotation - (prevRotation % 360);
+			if (isIncrementing) {
+				if (digit === '0' && rotationIncrement > 0) {
+					rotationIncrement -= 360;
+				}
+			} else {
+				if (digit === '9' && rotationIncrement > 0) {
+					rotationIncrement -= 360;
+				} else if (rotationIncrement < -36) {
+					rotationIncrement += 360;
+				}
+			}
+
+			return prevRotation + rotationIncrement;
+		});
+	}, [digit, isIncrementing]);
+
+	return (
+		<div className='dial' style={{ transform: `rotateX(${rotation}deg)` }}>
+			{[...Array(10)].map((_, index) => (
+				<div key={index} className='number' style={{ transform: `rotateX(${index * 36}deg) translateZ(50px)` }}>
+					{index}
+				</div>
+			))}
+		</div>
+	);
+};
 
 interface CountdownProps {
 	startNumber: number;
@@ -8,40 +47,33 @@ interface CountdownProps {
 
 const Countdown: React.FC<CountdownProps> = ({ startNumber, endNumber }) => {
 	const [currentNumber, setCurrentNumber] = useState(startNumber);
+	const isIncrementing = endNumber > startNumber;
 
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setCurrentNumber((prev) => {
-				if (prev === endNumber) {
-					clearInterval(interval);
-					return prev;
-				}
-				return endNumber > startNumber ? prev + 1 : prev - 1;
-			});
-		}, 1000); // Adjust time for faster/slower animation
+		const timeout = setTimeout(() => {
+			const interval = setInterval(() => {
+				setCurrentNumber((prev) => {
+					if ((isIncrementing && prev === endNumber) || (!isIncrementing && prev === endNumber)) {
+						clearInterval(interval);
+						return prev;
+					}
+					return prev + (isIncrementing ? 1 : -1);
+				});
+			}, 1000);
 
-		return () => clearInterval(interval);
-	}, [startNumber, endNumber]);
+			return () => clearInterval(interval);
+		}, 1000);
+
+		return () => clearTimeout(timeout);
+	}, [startNumber, endNumber, isIncrementing]);
 
 	const renderDials = () => {
-		const dials = [];
 		const numberString = currentNumber.toString().padStart(10, '0');
-		for (let i = 0; i < numberString.length; i++) {
-			const rotation = -36 * parseInt(numberString[i]);
-			dials.push(
-				<div key={i} className='dial' style={{ transform: `rotateX(${rotation}deg)` }}>
-					{Array.from({ length: 10 }).map((_, index) => (
-						<div key={index} className='number' style={{ transform: `rotateX(${index * 36}deg) translateZ(50px)` }}>
-							{index}
-						</div>
-					))}
-				</div>
-			);
-		}
-		return dials;
+
+		return numberString.split('').map((digit, i) => <Dial key={i} digit={digit} isIncrementing={isIncrementing} />);
 	};
 
-	return <div className='counter-slot-machine'>{renderDials()}</div>;
+	return <div className='countdown'>{renderDials()}</div>;
 };
 
 export default Countdown;
